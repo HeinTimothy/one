9d25649fe2d22871afde561f5cacc4a431953374
Timmy <timothyfhein@gmail.com>
2021-10-28 21:27:10 -0400

    styles?

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
